da63fb637ca6d11774c42b5630253022cb098603
maharengarajan <maharengarajan46@gmail.com>
2024-01-10 21:40:06 +0530

    working on data ingestion component

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from src.exception.exception import customexception
+from src.logger.logging import logging
+
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+
